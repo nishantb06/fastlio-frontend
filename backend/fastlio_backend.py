a37fbe4d48208749e7aa164453d8e2972bf5e7d9
nbhansali06@gmail.com <nbhansali06@gmail.com>
2025-04-04 13:02:01 +0530

    feat: dockerfile for backend

--- backend/fastlio_backend.py
+++ backend/fastlio_backend.py
@@ -283,3 +283,13 @@ async def update_robot(key_event: KeyEvent):
         landmark_eigenvals=landmark_eigenvals,
         landmark_angles=landmark_angles
     )
+
+
+# health check
+@app.get("/")
+async def health():
+    return {"status": "ok"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
